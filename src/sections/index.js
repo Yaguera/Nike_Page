919a925c94630f91f271c030358c14bd72557c60
Yaguera <ya-gotre@hotmail.com>
2024-03-05 20:07:36 -0300

    initial commit / NavComponent

---
+++ src/sections/index.js
@@ -0,0 +1,11 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts' 
+import Footer from './Footer'
+import Services from './Services'
+import CustomerReviews from './CustomerReviews'
+import SpecialOffer  from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from  "./SuperQuality"
+export {
+    Hero,PopularProducts,CustomerReviews,Footer,Services,SpecialOffer,Subscribe,SuperQuality
+}
